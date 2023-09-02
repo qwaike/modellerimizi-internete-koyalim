a868747045598fa9bbf716c9ecf1222bec5a4320
qwaike <57138812+qwaike@users.noreply.github.com>
2023-09-02 13:20:07 +0300

    consoledan bi log alalım

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import * as dat from 'dat.gui'
-
+console.log("object");
 /**
  * Base
  */
